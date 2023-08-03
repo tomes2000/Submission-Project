0ddab9be7e4fdebac6e27df6babaa5e89f4de595
tomes2000 <aavash2000@gmail.com>
2023-08-03 22:11:03 +0100

    add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,14 @@
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-    $('.collapsible').collapsible();
-    $('.tooltipped').tooltip();
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
-
