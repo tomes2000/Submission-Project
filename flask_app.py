cb6d464f74f9171196947e96faa49ec2dc32d8ed
tomes2000 <aavash2000@gmail.com>
2023-08-03 10:27:58 +0100

    adding render templates

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
