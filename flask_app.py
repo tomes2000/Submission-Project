7d53f62d6129a1449a8204d73d8c35cc33557184
tomes2000 <aavash2000@gmail.com>
2023-08-03 15:07:00 +0100

    login function implemented

--- flask_app.py
+++ flask_app.py
@@ -46,6 +46,11 @@ def register():
         flash("Registration Successful")
     return render_template("register.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
