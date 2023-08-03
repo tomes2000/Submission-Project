3dcbbddb48eae59208306495daa0a4f066f5d203
tomes2000 <aavash2000@gmail.com>
2023-08-03 10:35:40 +0100

    adding posts

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
