a53f8cc527af78941f92d1887669ba81ef50c61e
tomes2000 <aavash2000@gmail.com>
2023-08-03 10:26:58 +0100

    adding home and about page and @app.routes

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+app = Flask(__name__)
 
-app= Flask(__name__)
 
-@app.route('/')
-
+@app.route("/")
+@app.route("/home")
 def home():
-    return "Hello, World!"
+    return "<h1>Home Page</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
