c06898282ac88ee79ced3581d913a7a5b0cb474d
tomes2000 <aavash2000@gmail.com>
2023-08-05 09:43:14 +0100

    confirm delete modal implemented

--- static/js/script.js
+++ static/js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
     $("select").formSelect();
+    $('.modal').modal();
     $(".datepicker").datepicker({
         format: "dd mmmm, yyyy",
         yearRange: 3,
